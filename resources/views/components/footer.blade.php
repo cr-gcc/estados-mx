ff491d61e8a976080d1b08ba378e23377404afbf
cris-contact-cinko <cris.gcc@outlook.com>
2025-11-03 12:09:07 -0600

    Componentes

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white py-4">
+		<div class="container text-center">
+			<p class="mb-1">&copy; 2025 Estados MX</p>
+			<ul class="list-inline mb-0">
+				<li class="list-inline-item"><a href="#" class="text-white">Inicio</a></li>
+				<li class="list-inline-item"><a href="#" class="text-white">Acerca de</a></li>
+				<li class="list-inline-item"><a href="#" class="text-white">Contacto</a></li>
+			</ul>
+		</div>
+	</footer>
