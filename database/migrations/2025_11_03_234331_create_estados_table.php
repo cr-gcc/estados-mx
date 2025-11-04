6b0fae86cf2971c159111bb8c9436a3019819ca3
Cris GC <cris.gcc@outlook.com>
2025-11-03 23:34:50 -0600

    Ajustes de configuración

--- database/migrations/2025_11_03_234331_create_estados_table.php
+++ database/migrations/2025_11_03_234331_create_estados_table.php
@@ -6,27 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-	/**
-	 * Run the migrations.
-	 */
-	public function up(): void
-	{
-		Schema::create('estados', function (Blueprint $table) {
-			$table->id();
-			$table->char('clave_geo', 2);
-			$table->char('clave_area', 2);
-			$table->string('nombre', 128);
-			$table->char('abreviatura', 3);
-			$table->unsignedBigInteger('poblacion');
-			$table->timestamps();
-		});
-	}
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('estados', function (Blueprint $table) {
+      $table->id();
+      $table->char('clave_geo', 2);
+      $table->char('clave_area', 2);
+      $table->string('nombre', 128);
+      $table->string('abreviatura', 10);
+      $table->unsignedBigInteger('poblacion');
+      $table->timestamps();
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 */
-	public function down(): void
-	{
-		Schema::dropIfExists('estados');
-	}
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('estados');
+  }
 };
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+  return view('welcome');
+});
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/estados-inegi', [HomeController::class, 'estadosInegi']);
 Route::get('/carga-inegi', [HomeController::class, 'cargaInegi']);
